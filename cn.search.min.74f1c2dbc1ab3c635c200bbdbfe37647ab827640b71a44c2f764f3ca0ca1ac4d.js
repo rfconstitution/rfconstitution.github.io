75d4a1ac96fb523e227cb5b60f3a9b435fa11af1
Markin Sergey <smarkin@vrconcept.net>
2020-02-03 23:57:17 +0300

    rebuilding site  3 фев 2020 г. 23:57:17

---
+++ cn.search.min.74f1c2dbc1ab3c635c200bbdbfe37647ab827640b71a44c2f764f3ca0ca1ac4d.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/cn.search-data.min.b280b610ae3cfce1434b94f29540d2c3498b3e10f71dfd9ad46998be450d45d7.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
